4303b7b8d2dd0d8a4ea51919555bdb532f1d92eb
Joseph Kim <josek16@uci.edu>
2022-11-06 20:52:05 -0800

    update

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -14,16 +14,37 @@ class StudentAI():
         self.color = ''
         self.opponent = {1:2,2:1}
         self.color = 2
-    def get_move(self,move):
+
+    def get_move(self, move):
         if len(move) != 0:
             self.board.make_move(move,self.opponent[self.color])
         else:
             self.color = 1
-        moves = self.board.get_all_possible_moves(self.color)
 
-        # get row, col
-        index = randint(0,len(moves)-1)
-        inner_index =  randint(0,len(moves[index])-1)
-        move = moves[index][inner_index]
-        self.board.make_move(move,self.color)
-        return move
+        moves = self.board.get_all_possible_moves(self.color)
+        best_seq_score, best_seq = self.minimax(4)
+
+        self.board.make_move(best_seq, self.color)
+        return best_seq
+
+    def minimax(self, depth):
+        if depth == 0:
+            return self.board.black_count - self.board.white_count, None
+
+        if self.board.is_win(self.color):
+            return 100, None
+
+        moves = self.board.get_all_possible_moves(self.color)
+        best_seq_score = 0
+        best_seq = moves[0][0]
+
+        for possible_move in moves:
+            for possible_seq in possible_move:
+                self.board.make_move(possible_seq, self.color)
+                possible_seq_score, _ = self.minimax(depth - 1)
+                if possible_seq_score > best_seq_score:
+                    best_seq_score = possible_seq_score
+                    best_seq = possible_seq
+                self.board.undo()
+
+        return best_seq_score, best_seq
